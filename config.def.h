2995053f9353820cfe034329b4e522f006dd6c9a
Bark <kristoffer@nosslo.nu>
2022-04-30 00:24:20 +0200

    Add border patch

--- config.def.h
+++ config.def.h
@@ -30,3 +30,6 @@ static unsigned int lines	= 6;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
